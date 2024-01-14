a32909d39383267b445006c19435f5e11f15485e
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-01-14 13:03:26 -0300

    Aula 72

---
+++ aula072.py
@@ -0,0 +1,16 @@
+"""
+Exercícios com funções
+
+Crie uma função que multiplica todos os argumentos
+não nomeados não recebidos
+Retorne o total para uma variável e mostre
+o valor da variável
+"""
+def multiplicar(*args):
+    total = 0
+    for numero in args:
+        total *= numero
+    return total
+
+    multiplicacao = multiplicar(1, 2, 3, 4, 5)
+print(multiplicacao)    
