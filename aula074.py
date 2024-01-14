99cc9a5e976f4f594dfbd5b2472cc4eadfae6663
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-01-14 18:44:20 -0300

    modificacao aula 74

--- aula074.py
+++ aula074.py
@@ -1,14 +1,14 @@
 # Closure e funções que retornam outras funções
 
-def criar_saudadacao(saudacao, nome):
-    def saudar():
-        return f'{saudacao}, {nome}'
+def criar_saudadacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}!'
     return saudar
 
 
-s1 = criar_saudadacao('Bom dia', ' Luiz')
-s2 = criar_saudadacao('Boa noite', 'Luiz')
+falar_bom_dia = criar_saudadacao('Bom dia')
+falar_boa_noite = criar_saudadacao('Boa noite')
 
-
-print(s1())
-print(s2())
+for nome in ['Maria', 'Joana', 'Luiz']:
+    print(falar_bom_dia(nome))
+    print(falar_boa_noite(nome))
