f14526e86eb07f1464dac720b180e63643450024
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-01-14 13:06:48 -0300

    ajuste de problemas

--- aula072.py
+++ aula072.py
@@ -6,11 +6,13 @@ não nomeados não recebidos
 Retorne o total para uma variável e mostre
 o valor da variável
 """
+
+
 def multiplicar(*args):
     total = 0
     for numero in args:
         total *= numero
     return total
 
-    multiplicacao = multiplicar(1, 2, 3, 4, 5)
-print(multiplicacao)    
+multiplicacao = multiplicar(1, 2, 3, 4, 5)
+print(multiplicacao)
