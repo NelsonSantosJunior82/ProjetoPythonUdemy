cf638add5a320887e98dc86f6b90a370fd5b3290
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-01-14 18:00:09 -0300

    modificacao aula 72

--- aula072.py
+++ aula072.py
@@ -9,10 +9,30 @@ o valor da variável
 
 
 def multiplicar(*args):
-    total = 0
+    total = 1
     for numero in args:
         total *= numero
     return total
 
+
 multiplicacao = multiplicar(1, 2, 3, 4, 5)
 print(multiplicacao)
+
+"""
+Crie uma função fala se um número é par ou ìmpar
+Retorne se um número é par ou ímpar
+"""
+
+
+def par_impar(numero):
+
+    multiplo_de_dois = numero % 2 == 0
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    return f'{numero} é impar'
+
+
+print(par_impar(2))
+print(par_impar(3))
+print(par_impar(15))
+print(par_impar(16))
---
+++ aula073.py
@@ -0,0 +1,22 @@
+"""
+Higher order function
+Funções de primeira classe
+"""
+
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Maria')
+)
