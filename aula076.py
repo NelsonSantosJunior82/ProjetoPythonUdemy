d98a81d09b46f595abae34a9e6f65eaec4f02874
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-01-17 13:28:50 -0300

    Uso de mais formato de dicionário

---
+++ aula076.py
@@ -0,0 +1,35 @@
+"""
+Dicionários em Python (tipo dict)
+Dicionários são estrutura de dados do tipo
+par de chave "{}" e valor.
+Chaves podem ser consideradas como o "indíce"
+que vimos na lista e podem ser de tipo imutáveis
+como: str, int, float, bool, tuple, etc.
+O valor pode ser qualquer tipo, incluindo outro
+dicionário.
+Usamos chaves "{}" ou a classe dict para criar 
+dicionários.
+Imutáveis: str, int, float, bool, tuple
+Mutáveis: dict, list.
+
+"""
+"""
+pessoa = {
+    'nome': 'Nelson',
+    'sobrenome': 'Santos Júnior',
+    'idade': 41,
+    'altura': 1.67,
+    'endereço': [
+        {'Rua: Joaquim Teodoro Tavares, 46'},
+        {'Rua: Magalhães Lemos, 174'}]
+}
+"""
+pessoa = dict(
+    nome='Nelson',
+    sobrenome='Santos Júnior',
+    idade=41,
+    altura=1.67,
+    endereco=['Rua Joaquim Teodoro Tavares, 46',
+              'Rua Magalhães Lemos, 174']
+)
+print(pessoa)
