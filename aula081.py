1eb40d3cda6cf51291fd7c2de62e740f00fe20ed
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-02-26 14:59:28 -0300

    testando aula 81

--- aula081.py
+++ aula081.py
@@ -19,5 +19,18 @@ lista = [
     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
     {'nome': 'Daniel', 'sobrenome': 'Silva'},
     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
-    {'nome': 'Aline', 'sobrenme': 'Souza'}
+    {'nome': 'Aline', 'sobrenome': 'Souza'}
 ]
+
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
