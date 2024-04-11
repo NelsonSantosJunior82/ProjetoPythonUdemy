7c32d8f202f099159b88923494c3d7fa1e1f9cdc
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-04-10 21:58:05 -0300

    ponto de vista do main

--- aula099.py
+++ aula099.py
@@ -1,13 +1,21 @@
 # from sys import path
 
-import aula099_package.modulo
-from aula099_package import modulo
-from aula099_package.modulo import *
+# import aula099_package.modulo
+# from aula099_package import modulo
+# from aula099_package.modulo import *
 
 # from aula099_package.modulo import soma_do_modulo
 
 # print(*path, sep='\n')
-print(soma_do_modulo(1, 2))
-print(aula099_package.modulo.soma_do_modulo(1, 2))
-print(modulo.soma_do_modulo(1, 2))
-print(variavel)
+# print(soma_do_modulo(1, 2))
+# print(aula099_package.modulo.soma_do_modulo(1, 2))
+# print(modulo.soma_do_modulo(1, 2))
+# print(variavel)
+
+
+from aula099_package.modulo import soma_do_modulo, fala_oi
+
+
+print(__name__)
+fala_oi()
+soma_do_modulo(2, 3)
--- aula099_package/modulo.py
+++ aula099_package/modulo.py
@@ -3,8 +3,14 @@ __all__ = [
     'soma_do_modulo',
 ]
 
+from aula099_package.modulo_b import fala_oi
+
 variavel = 'Qualquer coisa'
 
 
 def soma_do_modulo(x, y):
     return x + y
+
+
+nova_variavel = 'OK'
+fala_oi()
