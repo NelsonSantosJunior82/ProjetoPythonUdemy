a4bd2f7a3808caccc9042aac202954b9a4a50103
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-04-12 16:48:55 -0300

    Aula 100 exercícios

---
+++ aula100.py
@@ -0,0 +1,15 @@
+# Exercícios
+# Aumente o preço dos produto em 10 por cento
+# Gere novos_produtos por deep copy(cópia continua)
+
+produtos = [
+    {'nome': 'Produto 5', 'preço': 10.00},
+    {'nome': 'Produto 1', 'preço': 22.32},
+    {'nome': 'Produto 3', 'preço': 10.11},
+    {'nome': 'Produto 2', 'preço': 105.87},
+    {'nome': 'Produto 4', 'preço': 69.90},
+
+]
+
+# Ordene os produtos por nome decrescente(do maior para menor)
+# Gere produtos_ordenados por preço poer deep copy(cópia profunda)
