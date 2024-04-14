617fb54089b5b08f095991ad48f6f4d1e85c2d3b
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-04-14 16:25:42 -0300

    Exercicio de funçôes

--- aula100.py
+++ aula100.py
@@ -1,15 +1,50 @@
+import copy
+
+from dados import produtos
+
 # Exercícios
 # Aumente o preço dos produto em 10 por cento
 # Gere novos_produtos por deep copy(cópia continua)
-
-produtos = [
-    {'nome': 'Produto 5', 'preço': 10.00},
-    {'nome': 'Produto 1', 'preço': 22.32},
-    {'nome': 'Produto 3', 'preço': 10.11},
-    {'nome': 'Produto 2', 'preço': 105.87},
-    {'nome': 'Produto 4', 'preço': 69.90},
-
+novos_produtos = [
+    {**p, 'preço': round(p['preço'] * 1.1, 2)}
+    for p in copy.deepcopy(produtos)
 ]
 
+# print(*produtos, sep='\n')
+# print("=" * 20)
+# print(*novos_produtos, sep='\n')
+
 # Ordene os produtos por nome decrescente(do maior para menor)
-# Gere produtos_ordenados por preço poer deep copy(cópia profunda)
+# Gere produtos_ordenados_por_nome por deep copy(cópia profunda)
+
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome'],
+    reverse=True
+)
+
+# print(*produtos, sep='\n')
+# print("=" * 20)
+# print(*produtos_ordenados_por_nome, sep='\n')
+# Ordene os produtos por nome crescente(do meno para maior)
+# Gere produtos_ordenados_por_preco por deep copy(cópia profunda)
+
+produtos_ordenados_por_preco = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['preço'],
+    reverse=True
+)
+
+# print(*produtos, sep='\n')
+# print("=" * 20)
+# print(*produtos_ordenados_por_preco, sep='\n')
+
+# FINAL
+
+print(*produtos, sep='\n')
+print("=" * 20)
+print(*novos_produtos, sep='\n')
+print("=" * 20)
+print(*produtos_ordenados_por_nome, sep='\n')
+print("=" * 20)
+print(*produtos_ordenados_por_preco, sep='\n')
