e5b2d745813423bd8dff21cd587ec7f5a9bac886
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-06-10 19:14:40 -0300

    Criação da aula 008
    Exercício sobre variáveis

---
+++ aula008.py
@@ -0,0 +1,13 @@
+nome = 'Nelson'
+sobrenome = 'Santos júnior'
+idade = 43
+nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.65
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
